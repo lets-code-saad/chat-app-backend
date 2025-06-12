1dc9badb8db99c816168ade34a5d35cb961cb9b5
Saad <saadabbas594@gmail.com>
2025-06-12 22:52:09 +0500

    backend cors err fix

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ app.use(
   cors({
     origin: [
       "https://chirpchat-beta.vercel.app",
-      "https://chirpchat-backend.vercel.app",
       "http://localhost:3000",
       "http://localhost:3001",
       "http://localhost:8000",
